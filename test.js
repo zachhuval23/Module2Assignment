09f6ed29722d449698cab1d9a56cfaaa0b1b56b8
zachhuval23 <zachhuval23@gmail.com>
2023-04-18 15:07:36 -0700

    Quick update for Module 2 Webpage

---
+++ test.js
@@ -0,0 +1,13 @@
+let firstName = "Zach";
+console.log("Hello World!");
+console.log("Hello " + firstName)
+
+let num = 10;
+
+if (num > 5) {
+  console.log("The number is greater than 5.");
+} else {
+  console.log("The number is less than or equal to 5.");
+}
+
+
